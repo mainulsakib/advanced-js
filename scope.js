ccd68d47efa258b42826d37f923f4b90d616eecb
mainulsakib <losersakib@gmail.com>
2020-08-08 13:18:26 +0600

    all

---
+++ scope.js
@@ -0,0 +1,18 @@
+// const sum = (x,y) =>x+y;
+// const give5=()=>5;
+// const result=sum(44,35);
+// const result2=give5();
+// console.log(result);
+// console.log(result2);
+function sum(first,second){
+ result=first+second;
+ if(result>9){
+     var mood="fishy";
+     mood="funky";
+     mood="cranky";
+     console.log(mood);
+ } 
+ console.log(mood);
+ return result;  
+}
+sum(4,9)
---
+++ slice.js
@@ -0,0 +1,6 @@
+const num=[4,56 ,35,24,22,45,54,5];
+const part=num.slice(2,5);
+const removed=num.splice(2,4,44,85);
+console.log(part);
+console.log(removed);
+console.log(num)
